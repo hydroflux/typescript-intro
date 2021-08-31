6d62fece349fb0ae02d049af03e13e17475146cf
Jack Hubert <jphubert93@gmail.com>
2021-08-31 17:04:31 -0600

    introduced interfaces (a contract for objects), creating an AppState interface 'IAppState' for state in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
-export class App extends Component<{}, {}> {
+// Interfaces indicate that an object must always have certain elements in it
+//  putting 'i' in front of an interface is common shorthand to indicate an interface
+//  interfaces are like contracts for objects
+
+interface IAppState {
+  name: string
+  characters: {}[]
+}
+
+export class App extends Component<{}, IAppState> {
 
   constructor( props: {} ) {
     super(props)
