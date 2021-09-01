8812f69d9781828d62ad2f0b6b6d563a5a4201f7
Jack Hubert <jphubert93@gmail.com>
2021-08-31 18:09:46 -0600

    moved interfaces comment into types index

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import CharacterCard from './components/CharacterCard';
 import { charactersURL, parseHTTPResponse } from './helpers/utilities';
 import { ICharacter } from './types';
 
-// Interfaces indicate that an object must always have certain elements in it
-//  putting 'i' in front of an interface is common shorthand to indicate an interface
-//  interfaces are like contracts for objects
-
 const App: React.FC<{}> = () => {
 
   const [ characters , setCharacters ] = useState<ICharacter[]>([])
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -16,3 +16,7 @@ interface IPointer {
     name: string,
     url: string
 }
+
+// Interfaces indicate that an object must always have certain elements in it
+//  putting 'i' in front of an interface is common shorthand to indicate an interface
+//  interfaces are like contracts for objects
